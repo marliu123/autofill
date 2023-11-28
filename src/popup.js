f905ce66ff2918478814ca5228c0362fb229701a
Marcus Liu <marcus.c.liu@gmail.com>
2023-11-28 16:44:24 -0500

    Edit

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,32 @@
+// src/popup.js
 document.addEventListener('DOMContentLoaded', function () {
-    
-    function handleAutofillButtonClick() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currentUrl = tabs[0]?.url || 'No URL found';
-        console.log('Button clicked! AutoFill event triggered on:', currentUrl);
-      });
-    }
-    document.getElementById('autofillButton')?.addEventListener('click', handleAutofillButtonClick);
-  });
-  
+  // Function to handle the button click event
+  function handleAutofillButtonClick() {
+    // Get the form values
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var address = document.getElementById('address').value;
+    var city = document.getElementById('city').value;
+    var country = document.getElementById('country').value;
+    var state = document.getElementById('state').value;
+    var zipCode = document.getElementById('zipCode').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+
+    // Log the form values to the console
+    console.log('Form submitted! Values:', {
+      firstName,
+      lastName,
+      address,
+      city,
+      country,
+      state,
+      zipCode,
+      phoneNumber,
+    });
+
+    // You can add additional logic here as needed
+  }
+
+  // Add an event listener to the button
+  document.getElementById('autofillButton')?.addEventListener('click', handleAutofillButtonClick);
+});
