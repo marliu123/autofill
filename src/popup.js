53020b004e4e86cdf19c35b044afb574f7676f4f
Marcus Liu <marcus.c.liu@gmail.com>
2023-11-28 23:49:46 -0500

    edit

--- src/popup.js
+++ src/popup.js
@@ -1,32 +1,74 @@
-// src/popup.js
 document.addEventListener('DOMContentLoaded', function () {
-  // Function to handle the button click event
-  function handleAutofillButtonClick() {
-    // Get the form values
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var address = document.getElementById('address').value;
-    var city = document.getElementById('city').value;
-    var country = document.getElementById('country').value;
-    var state = document.getElementById('state').value;
-    var zipCode = document.getElementById('zipCode').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
 
-    // Log the form values to the console
-    console.log('Form submitted! Values:', {
-      firstName,
-      lastName,
-      address,
-      city,
-      country,
-      state,
-      zipCode,
-      phoneNumber,
+  const personalInfoLink = document.getElementById('personalInfoLink');
+  const creditCardLink = document.getElementById('creditCardLink');
+  const personalInfoSection = document.getElementById('personalInfoSection');
+  const creditCardSection = document.getElementById('creditCardSection');
+  const autofillButton = document.getElementById('autofillButton');
+
+  showSection('personalInfo');
+
+
+  personalInfoLink.addEventListener('click', function () {
+    showSection('personalInfo');
+  });
+
+  creditCardLink.addEventListener('click', function () {
+    showSection('creditCard');
+  });
+
+  autofillButton.addEventListener('click', function () {
+
+    const personalInfoData = getFormData('personalInfoForm');
+    const creditCardData = getFormData('creditCardForm');
+
+    chrome.storage.sync.set({
+      personalInfo: personalInfoData,
+      creditCard: creditCardData,
+    }, function () {
+      console.log('Data saved:', personalInfoData, creditCardData);
     });
+  });
 
-    // You can add additional logic here as needed
+  function showSection(section) {
+    if (section === 'personalInfo') {
+      personalInfoSection.style.display = 'block';
+      creditCardSection.style.display = 'none';
+      personalInfoLink.classList.add('active');
+      creditCardLink.classList.remove('active');
+    } else if (section === 'creditCard') {
+      personalInfoSection.style.display = 'none';
+      creditCardSection.style.display = 'block';
+      personalInfoLink.classList.remove('active');
+      creditCardLink.classList.add('active');
+    }
+
+    chrome.storage.sync.get(['personalInfo', 'creditCard'], function (data) {
+      populateFormData('personalInfoForm', data.personalInfo);
+      populateFormData('creditCardForm', data.creditCard);
+    });
   }
 
-  // Add an event listener to the button
-  document.getElementById('autofillButton')?.addEventListener('click', handleAutofillButtonClick);
+  function getFormData(formId) {
+    const form = document.getElementById(formId);
+    const formData = {};
+
+    for (const input of form.elements) {
+      if (input.tagName === 'INPUT') {
+        formData[input.name] = input.value;
+      }
+    }
+
+    return formData;
+  }
+
+  function populateFormData(formId, data) {
+    const form = document.getElementById(formId);
+
+    for (const input of form.elements) {
+      if (input.tagName === 'INPUT' && data[input.name]) {
+        input.value = data[input.name];
+      }
+    }
+  }
 });
